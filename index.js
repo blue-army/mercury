242dd5e72d94d1ca6514434614cb02d7674b2e5f
Lucas Natraj <lnatraj@slb.com>
2019-01-07 12:40:04 -0600

    get commit info

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ var collectionId = "slackers";
 
 var host = process.env.DB_HOST;
 var key = process.env.DB_KEY;
+var vstsAuth = process.env.VSTS_AUTH;
 var client = new DocumentDBClient(host, { masterKey: key });
 
 module.exports = function (context, req) {
 
-    context.log(req)
+    // context.log(req);
 
     var docId = req.query.key;
     var dbLink = 'dbs/' + databaseId;
@@ -91,45 +92,108 @@ module.exports = function (context, req) {
                 var commitUrl = baseUrl + projectId + "/_git/" + repo + "/commit/" + commit + "?refName=" + ref;
                 var branchUrl = baseUrl + projectId + "/_git/" + repo + "?version=GB" + branch + "&_a=history";
 
-                var slack = {
-                    channel: channel,
-                    username: username,
-                    icon_url: icon,
-                    attachments: [{
-                        color: color,
-                        pretext: pretext,
-                        mrkdwn_in: [
-                            "pretext"
-                        ],
-                        fallback: fallback,
-                        fields: [],
-                        actions: [{
-                            type: "button",
-                            text: branch,
-                            url: branchUrl
-                        }, {
-                            type: "button",
-                            text: commit.slice(0, 7),
-                            url: commitUrl
-                        }]
-                    }]
-                }
+                // get commit info
+                // if (docId === 'NULL-REFERENCE') {
+                //     context.log("----- null-reference -----")
+                //     context.done();
+                //     return;
+                // }
+                
+                var commitInfoUrl = baseUrl + projectId + '/_apis/git/repositories/' + repo + '/commits/' + commit;
+                context.log("commit info url: " + commitInfoUrl);
 
-                // log slack message
-                context.log(JSON.stringify(slack))
-
-                request.post({
-                    url: hook,
+                request.get({
+                    url: commitInfoUrl,
                     headers: {
-                        "Content-Type": "application/json"
+                        "Content-Type": "application/json",
+                        "Authorization": "Basic " + vstsAuth
                     },
-                    body: slack,
-                    json: true
                 }, function (error, response, body) {
-                    context.log(error);
-                    context.log(JSON.stringify(response));
-                    context.log(body);
-                    context.done();
+                    if (error) {
+                        context.log("commit info (error): " + error);
+                        context.res = { status: 400, body: { message: 'Error retrieving commit details!' } };
+                        context.done();
+                        return;
+                    }
+
+                    // log
+                    context.log("commit info (response): " + JSON.stringify(response));
+                    context.log("commit info (body): " + body);
+
+                    var details = "";
+                    if (response.statusCode !== 404) {
+
+                        // process commit comment
+                        var commitInfo = JSON.parse(body);
+                        var comment = commitInfo.comment;
+                        context.log("commit comment: " + comment);
+
+                        var lines = comment.split('\n').filter(Boolean);
+
+                        var msg = lines[0];
+                        for (i = 1; i < lines.length; i++) {
+                            var piece = lines[i];
+
+                            // check for key:user
+                            var parts = piece.split(':');
+                            if (parts.length == 2) {
+                                
+                                // find user id
+                                var p = teamDoc.members.find(function(member) {
+                                    return (member.email === parts[1].toLowerCase());
+                                });
+
+                                piece = parts[0] + ": "
+                                if (p) {
+                                    piece = piece + "<@" + p.id + ">";
+                                } else {
+                                    piece = piece + parts[1];
+                                }
+                            }
+
+                            msg += "\n" + piece;
+                        }
+
+                        details = "\n" + "```" + msg + "```"
+                    }
+
+                    var slack = {
+                        channel: channel,
+                        username: username,
+                        icon_url: icon,
+                        attachments: [{
+                            color: color,
+                            fallback: fallback,
+                            fields: [],
+                            text: pretext + details,
+                            actions: [{
+                                type: "button",
+                                text: branch,
+                                url: branchUrl
+                            }, {
+                                type: "button",
+                                text: commit.slice(0, 7),
+                                url: commitUrl
+                            }]
+                        }]
+                    }
+
+                    // log slack message
+                    context.log(JSON.stringify(slack))
+
+                    request.post({
+                        url: hook,
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: slack,
+                        json: true
+                    }, function (error, response, body) {
+                        context.log("slack (error): " + error);
+                        context.log("slack (response): " + JSON.stringify(response));
+                        context.log("slack (body): " + body);
+                        context.done();
+                    });
                 });
             });
         }
